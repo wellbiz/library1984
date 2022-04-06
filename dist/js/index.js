f2f5b4de838db5f31697598f30109d484bbbc21b
Dmitry Taras <wellbiz@ya.ru>
2022-04-06 06:19:59 +0300

    залил папку dist

---
+++ dist/js/index.js
@@ -0,0 +1,13 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/modules/test.js
+var sum = function sum(a, b) {
+  return a + b;
+};
+;// CONCATENATED MODULE: ./src/js/index.js
+
+console.log(sum(1, 3));
+/******/ })()
+;
